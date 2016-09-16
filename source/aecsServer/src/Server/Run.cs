03681f6c8ac1a2acf03b746a272bbb9d81769986
Lennie Wennerlund <lennie@lempa.se>
2016-09-16 11:13:04 +0200

    Server class added

---
+++ source/aecsServer/src/Server/Run.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aecs.Server
+{
+    public class Run
+    {
+        public void Start()
+        {
+
+            Console.WriteLine("nice");
+            Console.ReadLine();
+
+        }
+    }
+}
