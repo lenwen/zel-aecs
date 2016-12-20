2ca33e2d17f5f6e0a1928a729ac40d1fa9b5cec1
Lennie Wennerlund <lennie@lempa.se>
2016-12-20 11:31:47 +0100

    testcode

---
+++ source/aecsServer/src/AecsIoT/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace AecsIoT
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Starting software");
+
+
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+
+    public static class Settings
+    {
+        public static Dictionary<string, ClientDataClass> Clients { get; set; }
+
+    }
+    public class ClientDataClass
+    {
+        public UInt64 Id { get; set; }
+
+    }
+}
